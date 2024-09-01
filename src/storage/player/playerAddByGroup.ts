db348cbf476c92d6b422e48dca1db063f8d06acf
Emanoel <emanoeljuvencio@gmail.com>
2024-09-01 00:07:19 -0300

    feat: criando metodo para buscar player por grupo, e melhorando o metodo de adicionar player no grupo.

--- src/storage/player/playerAddByGroup.ts
+++ src/storage/player/playerAddByGroup.ts
@@ -3,16 +3,29 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { PLAYER_COLLECTION } from '@storage/storageConfig'
 
 import { TPlayerStorageDTO } from './PlayerStorageDTO'
+import { playersGetByGroup } from './playersGetByGroup'
+import { AppError } from '@utils/AppError'
 
 export async function playerAddByGroup(
   newPlayer: TPlayerStorageDTO,
-  group: String
+  group: string
 ) {
   try {
-    await AsyncStorage.setItem(
-      `${PLAYER_COLLECTION}-${group}`,
-      JSON.stringify(newPlayer)
+    const storedPlayers = await playersGetByGroup(group)
+
+    const playerAlreadyExists = storedPlayers.filter(
+      (player) => player.name === newPlayer.name
     )
+
+    if (playerAlreadyExists.length > 0) {
+      throw new AppError(
+        'Esta pessoa já foi adicionado em um time deste grupo.'
+      )
+    }
+
+    const storage = JSON.stringify([...storedPlayers, newPlayer])
+
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage)
   } catch (error) {
     throw error
   }
